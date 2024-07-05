9fb12e69eb759d546081a91c85216c8a8fc1dec0
Raphael Van Well <rapha.well@outlook.com>
2024-07-05 16:04:21 -0300

    Add GetProducts endpoint and handler; minor formatting fix
    Added a new `GetProductsEndpoint.cs` file to define an endpoint for retrieving products, including a `GetProductsResponse` record and a route for handling GET requests to "/products". Introduced `GetProductsHandler.cs` to define the query and result records (`GetProductsQuery` and `GetProductsResult`) and a `GetProductsQueryHandler` class for handling the query. Also made a minor formatting change in `CreateProductEndpoint.cs` by removing an unnecessary character from the namespace declaration.

--- src/Service/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Service/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,4 +1,4 @@
-﻿pnamespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
